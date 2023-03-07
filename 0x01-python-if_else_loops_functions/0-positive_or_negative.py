42ee030a948bf6967b4ba49e7be40e36c025562c
Cleve-codes <clevemomany@gmail.com>
2023-03-07 05:06:23 -0800

    Positive or negative

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+new_num = 0;
+
+if (number > 0):
+    new_num = number
+    print(f"{new_num} is positive")
+
+elif (number == 0):
+    print(f"{number} is zero")
+
+elif (number < 0):
+    new_num = number
+    print(f"{new_num} is negative")
