dff0a7b44c3760a859848c652aefc0dd973c95d3
Cleve-codes <clevemomanyi@gmail.com>
2023-03-07 06:33:50 -0800

    Alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-#function to print lowercase chars
 for i in range(97, 123):
-    print(chr(i), end = " ")
+    asciitoChar = chr(i)
+    toalpha = "" + asciitoChar
+    print(f"{toalpha}", end = '')
