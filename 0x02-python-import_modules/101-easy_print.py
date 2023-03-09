6ec19c642f12c725871f013041c09e39fcc20856
Cleve-codes <clevemomanyi@gmail.com>
2023-03-09 09:01:49 -0800

    PRINT easy

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
